786723b10c0cf575d97848c54960dc78c8006848
Sam Merham <samehisaac@yahoo.com>
2021-03-15 10:46:55 -0700

    completed

---
+++ app.js
@@ -0,0 +1,26 @@
+const form = document.getElementById('form');
+const first = document.getElementById('first');
+const last = document.getElementById('last');
+const btn = document.getElementById('submit');
+const container = document.getElementById('container');
+const mainJoke = document.getElementById('joke-main');
+
+
+
+async function getJokes(firstName, lastName) {
+    const response = await axios.get(`http://api.icndb.com/jokes/random`, { params: { firstName, lastName } });
+    const joke = response.data.value.joke;
+    console.log(joke)
+    mainJoke.innerText = joke;
+    // container.append(mainJoke);
+}
+
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+    firstName = first.value;
+    lastName = last.value;
+    getJokes(firstName,lastName);
+    first.value = ''
+    last.value = ''
+    mainJoke.innerText = '';
+});
